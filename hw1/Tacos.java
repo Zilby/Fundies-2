03d9b135d88f6f3c64dfde3447882cc7fdb9dc3b
Zilby <azilby@gmail.com>
2016-01-20 17:55:22 -0500

    fixed tacos

--- hw1/Tacos.java
+++ hw1/Tacos.java
@@ -29,14 +29,14 @@ class Filled implements ITaco {
 
 // to represent examples of taco orders
 class ExamplesTacos {
-    ITaco order1 = new EmptyShell(true);
-    Filled carnitas1 = new Filled(order1, "carnitas");
-    Filled salsa1 = new Filled(order1, "salsa");
-    Filled lettuce1 = new Filled(order1, "lettuce");
-    Filled cheddarCheese1 = new Filled(order1, "cheddar cheese");
+    ITaco shell1 = new EmptyShell(true);
+    ITaco carnitas1 = new Filled(shell1, "carnitas");
+    ITaco salsa1 = new Filled(carnitas1, "salsa");
+    ITaco lettuce1 = new Filled(salsa1, "lettuce");
+    ITaco order1 = new Filled(lettuce1, "cheddar cheese");
 
-    ITaco order2 = new EmptyShell(false);
-    Filled veggies1 = new Filled(order1, "veggies");
-    Filled guacamole1 = new Filled(order1, "guacamole");
-    Filled sourCream1 = new Filled(order1, "sourCream");
+    ITaco shell2 = new EmptyShell(false);
+    ITaco veggies2 = new Filled(shell2, "veggies");
+    ITaco guacamole2 = new Filled(veggies2, "guacamole");
+    ITaco order2 = new Filled(guacamole2, "sourCream");
 }
