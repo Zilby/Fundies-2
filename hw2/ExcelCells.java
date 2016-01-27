b18aa24fd2ba5b2c6ad84f064e235ddc0e98980c
Zilby <azilby@gmail.com>
2016-01-26 20:51:32 -0500

    basically finished

--- hw2/ExcelCells.java
+++ hw2/ExcelCells.java
@@ -6,22 +6,20 @@ interface IData {
     int getArgs();
     int getFuns();
     int getDepth();
+    String getFormula(int depth);
 }
 
 class Formula implements IData {
     int number;
     Cell a;
     Cell b;
+    IFun fun;
     
-    Formula (Cell a, Cell b, String f){
+    Formula (Cell a, Cell b, IFun fun){
 	this.a = a;
 	this.b = b;
-	if(f == "mul"){
-	    this.number = (this.a.value() * this.b.value());
-	}else if(f == "mod"){
-	    this.number = (this.a.value() % this.b.value());
-	}else
-	    this.number = (this.a.value() + this.b.value());
+	this.fun = fun;
+	this.number = fun.form(this.a.value(), this.b.value());
     }
 
     public int getValue(){
@@ -41,6 +39,11 @@ class Formula implements IData {
 	    return 1 + this.a.formulaDepth();
 	else
 	    return 1 + this.b.formulaDepth();
+    }
+
+    public String getFormula(int depth){
+	return (this.fun.getName() + "(" + this.a.formula((depth - 1))
+		+ ", " + this.b.formula((depth - 1)) + ")");
     }
 }
 
@@ -65,6 +68,44 @@ class Number implements IData {
 
     public int getDepth(){
 	return 0;
+    }
+
+    public String getFormula(int depth){
+	return Integer.toString(number);
+    }
+}
+
+interface IFun {
+    int form(int a, int b);
+    String getName();
+}
+
+class Mul implements IFun {   
+    public int form(int a, int b){
+	return (a * b);
+    }
+    
+    public String getName(){
+	return "mul";
+    }
+}
+
+class Mod implements IFun {
+    public int form(int a, int b){
+	return (a % b);
+    }
+    public String getName(){
+	return "mod";
+    }
+}
+
+class Sum implements IFun {
+    public int form(int a, int b){
+	return (a + b);
+    }
+
+    public String getName(){
+	return "sum";
     }
 }
 
@@ -95,13 +136,23 @@ class Cell {
     int formulaDepth(){
 	return this.data.getDepth();
     }
+
+    String formula(int depth){
+	if(depth == 0)
+	    return (this.column + Integer.toString(this.row));
+	else
+	    return this.data.getFormula(depth);
+    }
 }
 
 // contains all examples for the program
 class ExamplesExcelCells {
 
     public static void main(String[] args){
-    
+	IFun sum = new Sum();
+	IFun mul = new Mul();
+	IFun mod = new Mod();
+	
 	IData a1data = new Number (25);
 	Cell cellA1 = new Cell ("A", 1, a1data);
     
@@ -117,34 +168,34 @@ class ExamplesExcelCells {
 	IData e1data = new Number (16);
 	Cell cellE1 = new Cell ("E", 1, e1data);
 
-	IData e2data = new Formula (cellE1, cellD1, "sum");
+	IData e2data = new Formula (cellE1, cellD1, sum);
 	Cell cellE2 = new Cell ("E", 2, e2data);
 
-	IData a3data = new Formula (cellA1, cellB1, "mul");
+	IData a3data = new Formula (cellA1, cellB1, mul);
 	Cell cellA3 = new Cell ("A", 3, a3data);
 
-	IData c2data = new Formula (cellA3, cellC1, "sum");
+	IData c2data = new Formula (cellA3, cellC1, sum);
 	Cell cellC2 = new Cell ("C", 2, c2data);
 
-	IData c4data = new Formula (cellE1, cellD1, "mul");
+	IData c4data = new Formula (cellE1, cellD1, mul);
 	Cell cellC4 = new Cell ("C", 4, c4data);
 
-	IData b3data = new Formula (cellE1, cellA3, "mod");
+	IData b3data = new Formula (cellE1, cellA3, mod);
 	Cell cellB3 = new Cell ("B", 3, b3data);
 
-	IData d2data = new Formula (cellC2, cellE2, "mod");
+	IData d2data = new Formula (cellC2, cellE2, mod);
 	Cell cellD2 = new Cell ("D", 2, d2data);
 
-	IData d3data = new Formula (cellD2, cellA1, "mul");
+	IData d3data = new Formula (cellD2, cellA1, mul);
 	Cell cellD3 = new Cell ("D", 3, d3data);
 
-	IData d4data = new Formula (cellC4, cellA1, "sum");
+	IData d4data = new Formula (cellC4, cellA1, sum);
 	Cell cellD4 = new Cell ("D", 4, d4data);
 
-	IData c5data = new Formula (cellD4, cellB3, "sum");
+	IData c5data = new Formula (cellD4, cellB3, sum);
 	Cell cellC5 = new Cell ("C", 5, c5data);
     
-	IData a5data = new Formula (cellD3, cellC5, "mod");
+	IData a5data = new Formula (cellD3, cellC5, mod);
 	Cell cellA5 = new Cell ("A", 5, a5data);
 
 	/* Example Cells
@@ -167,6 +218,11 @@ class ExamplesExcelCells {
 	System.out.println(cellB3.countFuns());
 	System.out.println(cellD2.formulaDepth());
 	System.out.println(cellB3.formulaDepth());
+	System.out.println(cellD2.formula(0));
+	System.out.println(cellD2.formula(1));
+	System.out.println(cellD2.formula(2));
+	System.out.println(cellD2.formula(3));
+	System.out.println(cellD2.formula(4));
     }
 }
 
@@ -229,7 +285,7 @@ class ExamplesExcelCells {
        
 	   IData c3data = new Formula (this.cellE2, this.cellB3, "sum");
 	   Cell cellC3 = new Cell ("C", 3, this.c3data);
-       */
+       
        }
 */
 
