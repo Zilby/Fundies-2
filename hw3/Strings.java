108ead631afb092a6b6207b63c749f758f86bb48
Zilby <azilby@gmail.com>
2016-02-03 18:02:43 -0500

    finished

--- hw3/Strings.java
+++ hw3/Strings.java
@@ -1,7 +1,11 @@
 // CS 2510 Fall 2014
 // Assignment 3
+// Nameth Kyle
+// kname5
+// Zilbersher Alexander
+// zilby
 
-//import tester.*;
+import tester.*;
 
 // to represent a list of Strings
 interface ILoString {
@@ -51,7 +55,7 @@ interface ILoString {
     
     // determines whether an ILoString is the same when observed from the
     // front or back of the list
-    boolean isPalindrome();
+    boolean isPalindromeList();
 }
 
 // to represent an empty list of Strings
@@ -85,35 +89,50 @@ class MtLoString implements ILoString {
 	return new ConsLoString(s, this);
     }
 
+    // determines whether a list is ordered alphabetically,
+    // treats all Strings as lowercase
     public boolean isSorted(){
 	return true;
     }
 
+    // returns whether or not this ILoString is the end of
+    // the list
     public boolean ended(){
 	return true;
     }
 
+    // takes an ILoString and produces a new ILoString where
+    // the first, third, fifth... elements are from this list,
+    // and the second, fourth, sixth... elements are from the given list.
     public ILoString interleave(ILoString l){
 	return l;
     }
 
+    // takes two sorted ILoStrings and combines them into
+    // one sorted ILoString
     public ILoString merge(ILoString l){
 	return l;
     }
 
+    // reverses the order of the Strings in this ILoString
     public ILoString reverse(){
 	return this;
     }
 
+    // assists the reverse() function by inserting a given
+    // String into the back of the list
     public ILoString reverseInsert(String s){
 	return new ConsLoString(s, this);
     }
 
+    // determines whether an ILoString is composed only of pairs of Strings
     public boolean isDoubledList(){
 	return true;
     }
 
-    public boolean isPalindrome(){
+    // determines whether an ILoString is the same when observed from the
+    // front or back of the list
+    public boolean isPalindromeList(){
 	return true;
     }
 	
@@ -161,10 +180,14 @@ class ConsLoString implements ILoString {
 	}
     }
 
+    // returns whether or not this ILoString is the end of
+    // the list
     public boolean ended(){
 	return false;
     }
-    
+
+    // determines whether a list is ordered alphabetically,
+    // treats all Strings as lowercase
     public boolean isSorted(){    
 	if ((!this.rest.ended()) &&
 	    (this.first.toLowerCase().compareTo(this.rest.getFirst().toLowerCase()) > 0)) {
@@ -174,24 +197,33 @@ class ConsLoString implements ILoString {
 	}
     }
 
+    // takes an ILoString and produces a new ILoString where
+    // the first, third, fifth... elements are from this list,
+    // and the second, fourth, sixth... elements are from the given list.
     public ILoString interleave(ILoString l){
 	if (!l.ended()){
 	    this.rest = new ConsLoString(l.getFirst(), this.rest.interleave(l.getRest()));
 	} return this;
     }
 
+    // takes two sorted ILoStrings and combines them into
+    // one sorted ILoString
     public ILoString merge(ILoString l){
 	return this.rest.merge(l.insert(this.first));
     }
 
+    // reverses the order of the Strings in this ILoString
     public ILoString reverse(){
 	return this.rest.reverse().reverseInsert(this.first);
     }
 
+    // assists the reverse() function by inserting a given
+    // String into the back of the list
     public ILoString reverseInsert(String s){
 	return new ConsLoString(this.first, this.rest.reverseInsert(s));
     }
 
+    // determines whether an ILoString is composed only of pairs of Strings
     public boolean isDoubledList(){
 	if (this.first.toLowerCase().compareTo(this.rest.getFirst().toLowerCase()) == 0) {
 	    return this.rest.getRest().isDoubledList();
@@ -200,14 +232,16 @@ class ConsLoString implements ILoString {
 	}
     }
 
-    public boolean isPalindrome(){
+    // determines whether an ILoString is the same when observed from the
+    // front or back of the list
+    public boolean isPalindromeList(){
 	return this.interleave(this.reverse()).isDoubledList();
     }
 }
 
-// to represent examples for lists of strings
-
-class Driver{
+// This was for my personal testing purposes,
+// Should be deleted before submitting
+/*class Driver{
     public static void main(String[] args){
     
 	ILoString mary = new ConsLoString("Mary ",
@@ -256,15 +290,74 @@ class Driver{
 	//System.out.println(doubleTrue.isDoubledList());
 	//System.out.println(mary.isDoubledList());
 	//System.out.println(doubleFalse.isDoubledList());
-	//System.out.println(mary.isPalindrome());
-	//System.out.println(p1.isPalindrome());
-	//System.out.println(p2.isPalindrome());
+	//System.out.println(mary.isPalindromeList());
+	//System.out.println(p1.isPalindromeList());
+	//System.out.println(p2.isPalindromeList());
     }
+}
+*/
+
+// to represent examples for lists of strings
+Class ExamplesStrings{
+
+    ILoString mary = new ConsLoString("Mary ",
+			   new ConsLoString("had ",
+			     new ConsLoString("a ",
+			       new ConsLoString("little ",
+				 new ConsLoString("lamb.", new MtLoString())))));
+
+    ILoString joseph = new ConsLoString("jj ",
+			 new ConsLoString("ate ",
+		           new ConsLoString("bagels ", new MtLoString())));
+
+    ILoString doubleTrue = new ConsLoString("la ",
+	         	     new ConsLoString("la ",
+			       new ConsLoString("de ",
+	         		 new ConsLoString("de ",
+				   new ConsLoString("da ",
+				     new ConsLoString("da ", new MtLoString()))))));
+
+    ILoString doubleFalse = new ConsLoString("la ",
+			      new ConsLoString("la ",
+			        new ConsLoString("de ",
+				  new ConsLoString("de ",
+				    new ConsLoString("da ", new MtLoString())))));
+
+    ILoString p1 = new ConsLoString("la ",
+		     new ConsLoString("de ",
+		       new ConsLoString("da ",
+	       	         new ConsLoString("da ",
+       			   new ConsLoString("de ",
+			     new ConsLoString("la ", new MtLoString()))))));
+
+    ILoString p2 = new ConsLoString("la ",
+		     new ConsLoString("de ",
+		       new ConsLoString("da ",
+	       		 new ConsLoString("de ",
+       			   new ConsLoString("la ", new MtLoString())))));
+
+    // These are good tests to make sure everything works:
+    
+    //mary.sort().combine(); -> "a had lamb.little Mary "
+    //mary.isSorted(); -> false
+    //mary.sort().isSorted(); -> true
+    //mary.interleave(joseph).combine(); -> "Mary jj had ate a bagels little lamb."
+    //joseph.interleave(mary).combine(); -> "jj Mary ate had bagels a little lamb."
+    //mary.sort().merge(joseph.sort()).combine(); -> "a ate bagels had jj lamb.little Mary "
+    //mary.reverse().combine(); -> "lamb.little a had Mary "
+    //doubleTrue.isDoubledList(); -> true
+    //mary.isDoubledList(); -> false
+    //doubleFalse.isDoubledList(); -> false 
+    //mary.isPalindromeList(); -> false
+    //p1.isPalindromeList(); -> true
+    //p2.isPalindromeList(); -> true
+
+    // Uncomment this for testing
     // test the method combine for the lists of Strings
     /*boolean testCombine(Tester t){
         return 
             t.checkExpect(this.mary.combine(), "Mary had a little lamb.");
     }
     */
-}
 
+}
